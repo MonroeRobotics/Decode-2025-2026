733100fd9ae0aa657d6572a616742a6ddab3f73c
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2025-10-03 16:24:57 -0400

    chchgfhgvhgvhgvhg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ArmController.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.util;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ArmController {
+    Servo launchAngleServo;
+    Servo intakeServo;
+
+    DcMotor intakeAdvanceMotor;
+    DcMotor launchMotorL;
+    DcMotor launchMotorR;
+
+}
