712f7ff6d947927807b00cb112f324e4d3300d70
MonroeRobotics <144491371+MonroeRobotics@users.noreply.github.com>
2025-10-10 16:16:09 -0400

    chchgfhgvhgvhgvhg

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/launchTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class launchTest {
+    HardwareMap hardwareMap;
+    public DcMotorEx motorR;
+    public DcMotorEx motorL;
+
+
+}
