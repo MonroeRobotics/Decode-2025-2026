c9ff3022d0b15d9056937da68c4918528f9f0026
ZemerGrey <declan.s.rice2011@gmail.com>
2025-10-18 13:01:56 -0400

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/launchTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/launchTest.java
@@ -1,20 +1,15 @@
-package org.firstinspires.ftc.teamcode.util;
+package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+
 @TeleOp(name = "shotTest", group = "main")
 public class launchTest extends OpMode{
-
-    HardwareMap hardwareMap;
-    Gamepad gamepad;
     public DcMotorEx motorR;
     public DcMotorEx motorL;
 
@@ -29,18 +24,16 @@ public class launchTest extends OpMode{
         motorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        motorL.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        gamepad = new Gamepad();
+        motorR.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        if (gamepad.x){
+        if (gamepad1.x){
             motorL.setPower(0.45);
             motorR.setPower(0.45);
         }
-        if (gamepad.a){
+        if (gamepad1.a){
             motorL.setPower(0);
             motorR.setPower(0);
         }
