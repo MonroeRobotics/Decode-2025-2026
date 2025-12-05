0b5023ef318782283ae007041fbd09ee19b3b11f
ZemerGrey <declan.s.rice2011@gmail.com>
2025-12-05 16:52:47 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/redAuto.java
@@ -1,30 +1,27 @@
 package org.firstinspires.ftc.teamcode.auto;
-import androidx.annotation.NonNull;
 
 // RR-specific imports
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
 
 // Non-RR imports
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+        import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.util.ArmController;
 
 @Config
-@Autonomous(name = "blue auto", group = "Autonomous")
-public class blueAuto extends LinearOpMode{
+@Autonomous(name = "red auto", group = "Autonomous")
+public class redAuto extends LinearOpMode{
     ArmController armController;
     Gamepad previousGamepad;
 
@@ -34,9 +31,10 @@ public class blueAuto extends LinearOpMode{
         shoot
 
     }
+    Action path;
 
-    Vector2d blueStart = new Vector2d(60,-12); //90
-    Vector2d redStart = new Vector2d(60, 12); //90
+    Pose2d blueStart = new Pose2d(60,-12, Math.toRadians(90)); //90
+    Pose2d redStart = new Pose2d(60, 12, Math.toRadians(180)); //90
 
     Vector2d blueFarShot = new Vector2d(56,-12); //203
     Vector2d blueHumanPlayer = new Vector2d(56, -54); //180
@@ -53,6 +51,7 @@ public class blueAuto extends LinearOpMode{
     Vector2d redPickup1 = new Vector2d(35.5, 50); //90
     Vector2d redPickup2 = new Vector2d(11.5, 50); //90
     Vector2d redPickup3 = new Vector2d(-12, 50); //90
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
@@ -62,6 +61,12 @@ public class blueAuto extends LinearOpMode{
         armController.initArm();
 
         previousGamepad = new Gamepad();
-        mecanumDrive = new MecanumDrive(hardwareMap,);
+        mecanumDrive = new MecanumDrive(hardwareMap, redStart);
+
+        path = mecanumDrive.actionBuilder(redStart)
+                .strafeToLinearHeading(new Vector2d(0, 12), Math.toRadians(180)).build();
+
+        Actions.runBlocking(new SequentialAction(path));
+
     }
 }
