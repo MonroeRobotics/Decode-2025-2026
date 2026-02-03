c8facefed49cec777e605ebf086e572dea20c711
ZemerGrey <declan.s.rice2011@gmail.com>
2026-02-03 17:11:29 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/blueAutoClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/blueAutoClose.java
@@ -19,17 +19,17 @@ import org.firstinspires.ftc.teamcode.util.ArmController;
 
 @Config
 @Autonomous(name = "blue auto", group = "Autonomous")
-public class blueAuto extends LinearOpMode{
+public class blueAutoClose extends LinearOpMode{
     ArmController armController;
     Gamepad previousGamepad;
 
     MecanumDrive mecanumDrive;
 
-    Pose2d blueStart = new Pose2d(-40,-44, Math.toRadians(180)); //90
+    Pose2d blueStart = new Pose2d(-40,-44, Math.toRadians(203)); //90
     Vector2d blueCloseShot = new Vector2d(-40,-44); //203
     Vector2d blueCloseShotAdvance  = new Vector2d(-40, -49); //123
     Vector2d blueCloseShotTransition = new Vector2d(-15, -20); //123
-    Vector2d blueStop = new Vector2d(-20, -50); //90
+    Vector2d blueStop = new Vector2d(35.5, -32); //90
     Vector2d bluePickupLineup1 = new Vector2d(35.5, -32); //90
     Vector2d bluePickupLineup2 = new Vector2d(11.5, -32); //90
     Vector2d bluePickupLineup3 = new Vector2d(-12, -32); //90
@@ -48,16 +48,12 @@ public class blueAuto extends LinearOpMode{
         TRUE_STOP
 
     }
-    AutoState autoState = blueAuto.AutoState.SHOT;
+    AutoState autoState = blueAutoClose.AutoState.SHOT;
 
     long shotWaitTimer;
     boolean shotTimerStarted = false;
 
-
-    TrajectoryActionBuilder toShot;
-    TrajectoryActionBuilder toShotLeave;
-
-    TrajectoryActionBuilder toStop;
+    int cycleNum = 0;
 
 
     @Override
@@ -112,21 +108,29 @@ public class blueAuto extends LinearOpMode{
                     // Leave SHOT after 11 seconds total (increased slightly to accommodate gaps)
                     if (elapsed >= 11000) {
                         shotTimerStarted = false;
-                        autoState = blueAuto.AutoState.SHOT_LEAVE;
+                        autoState = blueAutoClose.AutoState.SHOT_LEAVE;
                     }
                     break;
                 case SHOT_LEAVE:
                     shotTimerStarted = false;
                     armController.currentArmState = ArmController.armState.rest;
+                    if (cycleNum == 0){
+                        TrajectoryActionBuilder pickUp1 = mecanumDrive.actionBuilder(mecanumDrive.localizer.getPose())
+                                .strafeToLinearHeading(blueCloseShotTransition, Math.toRadians(180))
+                                .strafeToLinearHeading(bluePickupLineup2, Math.toRadians(180))
+                                .strafeToLinearHeading(bluePickup2, Math.toRadians(180))
+                                .strafeToLinearHeading(bluePickupLineup2, Math.toRadians(180))
+                                .strafeToLinearHeading(blueCloseShotTransition, Math.toRadians(203));
+                        Actions.runBlocking(pickUp1.build());
+                    }
+                    else{
+                        TrajectoryActionBuilder leaveAction = mecanumDrive.actionBuilder(mecanumDrive.localizer.getPose())
+                                .strafeToLinearHeading(blueStop, Math.toRadians(180));
 
-                    // 1. Build the trajectory
-                    TrajectoryActionBuilder leaveAction = mecanumDrive.actionBuilder(mecanumDrive.localizer.getPose())
-                            .strafeToLinearHeading(bluePickupLineup1, Math.toRadians(90));
-
-                    // 2. RUN the trajectory (This is the missing step)
-                    Actions.runBlocking(leaveAction.build());
-
-                    autoState = blueAuto.AutoState.STOP;
+                        // 2. RUN the trajectory (This is the missing step)
+                        Actions.runBlocking(leaveAction.build());
+                    }
+                    autoState = blueAutoClose.AutoState.STOP;
                     break;
                 case STOP:
                     break;
